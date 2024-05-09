65bd9850f2e7dbdeb0557d3cdd54f30d7d9473de
matthewsr71 <167122042+matthewsr71@users.noreply.github.com>
2024-05-09 13:29:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["PLAYING RAINBOW SEIGE SIX","WATCHING MANCHESTER LIFE ROLEPLAY"];
 
 
 let currentIndex = 0;
