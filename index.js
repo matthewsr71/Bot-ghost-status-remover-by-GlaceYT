475ab12554f04c4732f8ae6f6a56612263fbd11b
matthewsr71 <167122042+matthewsr71@users.noreply.github.com>
2024-05-09 13:16:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["In Queue For Tom Clancy's Lane Boy","Watching Manchester Life Roleplay"];
 
 
 let currentIndex = 0;
